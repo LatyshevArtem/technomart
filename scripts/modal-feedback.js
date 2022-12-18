d667ed330a988a56e76b480ded1496fadc22b847
LatyshevArtem <artem.l.2002@mail.ru>
2022-12-18 22:25:08 +1000

    Added animation when submitting a feedback form failed
    Added shake animation when submitting a form with at least one empty field. Renamed the variable containing the author's name to a more explicit name. Added appropriate script for adding and removing animation class and preventing wrong form submission

--- scripts/modal-feedback.js
+++ scripts/modal-feedback.js
@@ -1,15 +1,29 @@
 const feedbackPopupOpenButtons = document.querySelectorAll('.feedback-open-button')
 const feedbackPopup = document.querySelector('.modal-feedback')
+const feedbackForm = feedbackPopup.querySelector('.feedback-form')
+const authorName = feedbackForm.querySelector('#feedback-author-name')
+const authorEmail = feedbackForm.querySelector('#feedback-author-email')
+const authorText = feedbackForm.querySelector('#feedback-author-text')
 const feedbackPopupCloseButton = feedbackPopup.querySelector('.modal-close')
-const feedbackPopupFirstInputField = feedbackPopup.querySelector('#feedback-author-name')
+
 
 for (let i = 0; i < feedbackPopupOpenButtons.length; i++) {
   feedbackPopupOpenButtons[i].addEventListener('click', () => {
     feedbackPopup.classList.add('modal-show')
-    feedbackPopupFirstInputField.focus();
+    authorName.focus();
   })
 }
 
+feedbackForm.addEventListener('submit', event => {
+  if (!authorName.value || !authorEmail.value || !authorText.value) {
+    event.preventDefault()
+    feedbackPopup.classList.remove('modal-error')
+    feedbackPopup.offsetWidth = feedbackPopup.offsetWidth
+    feedbackPopup.classList.add('modal-error')
+  }
+})
+
 feedbackPopupCloseButton.addEventListener('click', () => {
+  feedbackPopup.classList.remove('modal-error')
   feedbackPopup.classList.remove('modal-show')
 })
